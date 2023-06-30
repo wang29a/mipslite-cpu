a1ae089180600085271c2f48b58fc74501ceeb80
bridge71 <1415806690@qq.com>
2023-06-30 11:09:22 +0800

    add some instrctions

--- sources/alu.v
+++ sources/alu.v
@@ -2,10 +2,13 @@
 module alu(
     input [31:0] SrcA, SrcB,
     input [`ALU_CONTROL_LENGTH-1:0] alu_cont,
-    output zero,
-    output reg [31:0] ALUout
+    input [`LENGTH-1:0] LO, HI,
+    output reg [31:0] ALUout,
+    output reg [31:0] Write_LO, Write_HI,
+    output reg W_HILO,
+    output zero
 );
-
+reg [63:0] temp;
 assign zero = (ALUout == 32'h00000000) ? 1 : 0;
 
 always @(*) begin
@@ -18,6 +21,45 @@ always @(*) begin
             ALUout <= SrcA - SrcB;
         `ALU_CONTROL_ORI:
             ALUout <= SrcA | SrcB;
+        `ALU_CONTROL_AND:
+            ALUout <= SrcA & SrcB;
+        `ALU_CONTROL_NOR:
+            ALUout <= ~(SrcA | SrcB);
+        `ALU_CONTROL_XOR:
+            ALUout <= SrcA ^ SrcB;
+        `ALU_CONTROL_OR:
+            ALUout <= SrcA | SrcB;
+        `ALU_CONTROL_SLLV:
+            ALUout <= SrcB << SrcA;
+        `ALU_CONTROL_SRAV:
+            ALUout <= SrcB >>> SrcA;
+        `ALU_CONTROL_SRLV:
+            ALUout <= SrcB >> SrcA;
+        `ALU_CONTROL_MULTU:begin
+            temp <= SrcA * SrcB;
+            Write_LO <= temp[31:0];
+            Write_HI <= temp[63:32];
+            W_HILO <= 1'b1;
+        end
+        `ALU_CONTROL_DIVU:begin
+            Write_LO <= SrcA / SrcB;
+            Write_HI <= SrcA % SrcB;
+            W_HILO <= 1'b1;
+        end
+        `ALU_CONTROL_MFHI:
+            ALUout <= HI;
+        `ALU_CONTROL_MFLO:
+            ALUout <= LO;
+        `ALU_CONTROL_MTHI:begin
+            Write_HI <= SrcA;
+            Write_LO <= LO;
+            W_HILO <= 1'b1;
+        end
+        `ALU_CONTROL_MTLO:begin
+            Write_LO <= SrcA;
+            Write_HI <= HI;
+            W_HILO <= 1'b1;
+        end
         default:
             ALUout <= 0; 
     endcase
--- sources/head.v
+++ sources/head.v
@@ -17,21 +17,50 @@
 `define OP_SW           6'b101011
 `define OP_BEQ          6'b000100
 `define OP_JAL          6'b000011
+`define OP_XORI         6'b001110
 
 //func
 `define FUNC_ADDU       6'b100001
 `define FUNC_SUBU       6'b100011
+`define FUNC_AND        6'b100100
+`define FUNC_XOR        6'b100110
+`define FUNC_OR         6'b100101
+`define FUNC_NOR        6'b100111
+`define FUNC_SLLV       6'b000100
+`define FUNC_SRAV       6'b000111
+`define FUNC_SRLV       6'b000110
+`define FUNC_MULTU      6'b011001 
+`define FUNC_DIVU       6'b011011 
+`define FUNC_MFHI       6'b010000 
+`define FUNC_MFLO       6'b010010
+`define FUNC_MTHI       6'b010001
+`define FUNC_MTLO       6'b010011 
+
 //ALU OP
-`define ALU_OP_ADD          2'b00
-`define ALU_OP_SUB          2'b01
-`define ALU_OP_ORI          2'b10
-`define ALU_OP_R_TYPE       2'b11
+`define ALU_OP_ADD          3'b000
+`define ALU_OP_SUB          3'b001
+`define ALU_OP_ORI          3'b010
+`define ALU_OP_R_TYPE       3'b011
+`define ALU_OP_XORI         3'b100
 
-`define ALU_CONTROL_ADD     3'b000
-`define ALU_CONTROL_ADDU    3'b001
-`define ALU_CONTROL_SUBU    3'b010
-`define ALU_CONTROL_ORI     3'b011
-
+`define ALU_CONTROL_ADD     5'b00000
+`define ALU_CONTROL_ADDU    5'b00001
+`define ALU_CONTROL_SUBU    5'b00010
+`define ALU_CONTROL_ORI     5'b00011
+`define ALU_CONTROL_AND     5'b00100
+`define ALU_CONTROL_OR      5'b00101 
+`define ALU_CONTROL_NOR     5'b00111 
+`define ALU_CONTROL_XOR     5'b01000 
+`define ALU_CONTROL_SLLV    5'b01001 
+`define ALU_CONTROL_SRAV    5'b01010 
+`define ALU_CONTROL_SRLV    5'b01011 
+`define ALU_CONTROL_XORI    5'b01100
+`define ALU_CONTROL_MULTU   5'b01101 
+`define ALU_CONTROL_DIVU    5'b01111
+`define ALU_CONTROL_MTHI    5'b10000
+`define ALU_CONTROL_MTLO    5'b10001 
+`define ALU_CONTROL_MFHI    5'b10010 
+`define ALU_CONTROL_MFLO    5'b10011
 //EXTEND OP
 `define EXT_OP_SIGNED   1'b0
 `define EXT_OP_UNSIGNED 1'b1
@@ -41,3 +70,4 @@
 //define initial values
 `define INITIAL_VAL_32      32'h00000000
 `define INITIAL_VAL_6       6'b000000
+
